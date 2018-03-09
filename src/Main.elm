fef87aaf2d15e0b812db57344fa11f08790563d0
Kofi Gumbs <h.kofigumbs@gmail.com>
2018-03-08 23:04:58 -0600

    Make it look a lot more like a spreadsheet

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,6 @@
 module Main exposing (..)
 
+import Char
 import Dom
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -8,7 +9,7 @@ import Sheet
 import Task
 
 
----- MODEL ----
+-- MODEL
 
 
 type alias Model =
@@ -21,8 +22,8 @@ type alias Model =
 
 init : ( Model, Cmd Msg )
 init =
-    ( { rows = 20
-      , columns = 5
+    ( { rows = 100
+      , columns = 26
       , selected = Sheet.Position 1 1
       , sheet = Sheet.empty
       }
@@ -42,7 +43,7 @@ focusForumulaInput =
 
 
 
----- UPDATE ----
+-- UPDATE
 
 
 type Msg
@@ -65,27 +66,24 @@ update msg model =
 
 
 
----- VIEW ----
+-- VIEW
 
 
 view : Model -> Html Msg
 view model =
-    let
-        offset =
-            "30px"
-    in
     main_
-        []
+        [ style
+            [ ( "display", "flex" )
+            , ( "flex-direction", "column" )
+            , ( "height", "100vh" )
+            , ( "font-family", "sans-serif" )
+            ]
+        ]
         [ div
-            [ let
-                segmentation =
-                    "25px"
-              in
-              style
+            [ style
                 [ ( "display", "flex" )
-                , ( "padding", segmentation )
-                , ( "width", "calc(100vw - (2 * " ++ segmentation ++ "))" )
-                , ( "background-color", "#F5F5F5" )
+                , ( "padding", formulaPad )
+                , ( "width", "calc(100vw - (2 * " ++ formulaPad ++ "))" )
                 ]
             ]
             [ span
@@ -107,62 +105,152 @@ view model =
                 ]
                 []
             ]
-        , table
+        , div
             [ style
-                [ ( "table-layout", "fixed" )
-                , ( "border-collapse", "collapse" )
+                [ ( "flex", "1" )
+                , ( "overflow", "scroll" )
                 ]
             ]
-            (grid model)
+            [ table
+                [ style
+                    [ ( "width", "100%" )
+                    , ( "table-layout", "fixed" )
+                    , ( "border-collapse", "collapse" )
+                    ]
+                ]
+                (grid model)
+            ]
         ]
 
 
 grid : Model -> List (Html Msg)
 grid model =
-    loop model.rows <|
+    axis (letterLabels model.columns model.selected) model.rows <|
         \row ->
             tr [] <|
-                loop model.columns <|
+                axis (numberLabel row model.selected) model.columns <|
                     \column ->
+                        let
+                            current =
+                                Sheet.Position row column
+                        in
                         cell
-                            { current = Sheet.Position row column
-                            , selected = model.selected
-                            }
-                            model.sheet
+                            [ dataStyle current model.selected
+                            , onClick <| SelectCell current
+                            ]
+                            [ Sheet.render current model.sheet ]
 
 
-cell : { current : Sheet.Position, selected : Sheet.Position } -> Sheet.Cells -> Html Msg
-cell { current, selected } sheet =
-    td
-        [ style
-            [ ( "border"
-              , if current == selected then
-                    "double 1px #4184F5"
-                else
-                    "solid 1px #DADADA"
-              )
-            , ( "width", "100px" )
+letterLabels : Int -> Sheet.Position -> Html msg
+letterLabels amount (Sheet.Position _ column) =
+    tr [] <|
+        axis cornerCell amount <|
+            \index ->
+                cell
+                    [ labelStyle <| labelColor index column ]
+                    [ text <| String.fromChar <| Char.fromCode <| index + 64 ]
+
+
+numberLabel : Int -> Sheet.Position -> Html msg
+numberLabel index (Sheet.Position row _) =
+    cell
+        [ labelStyle <| labelColor index row ]
+        [ text <| toString index ]
+
+
+cornerCell : Html msg
+cornerCell =
+    cell [ labelStyle lightGrey ] [ text "" ]
+
+
+cell : List (Attribute msg) -> List (Html msg) -> Html msg
+cell attributes =
+    td <|
+        style
+            [ ( "width", "100px" )
+            , ( "max-width", "100px" )
+            , ( "padding", "5px" )
+            , ( "overflow", "hidden" )
+            , ( "white-space", "nowrap" )
             ]
-        , onClick <| SelectCell current
+            :: attributes
+
+
+dataStyle : Sheet.Position -> Sheet.Position -> Attribute msg
+dataStyle current selected =
+    if current == selected then
+        style
+            [ ( "border", "double 1px " ++ highlightBlue )
+            , ( "text-align", "left" )
+            ]
+    else
+        style
+            [ ( "border", "solid 1px " ++ mediumGray )
+            , ( "text-align", "right" )
+            ]
+
+
+labelStyle : String -> Attribute msg
+labelStyle color =
+    style
+        [ ( "text-align", "center" )
+        , ( "background-color", color )
+        , ( "border", "double 1px " ++ darkGrey )
         ]
-        [ Sheet.render current sheet ]
 
 
-loop : Int -> (Int -> a) -> List a
-loop =
-    loopHelp []
+labelColor : Int -> Int -> String
+labelColor current selected =
+    if current == selected then
+        mediumGray
+    else
+        lightGrey
 
 
-loopHelp : List a -> Int -> (Int -> a) -> List a
-loopHelp acc n func =
+formulaPad : String
+formulaPad =
+    "35px"
+
+
+axis : a -> Int -> (Int -> a) -> List a
+axis head n func =
+    head :: axisHelp [] n func
+
+
+axisHelp : List a -> Int -> (Int -> a) -> List a
+axisHelp acc n func =
     if n == 0 then
         acc
     else
-        loopHelp (func n :: acc) (n - 1) func
+        axisHelp (func n :: acc) (n - 1) func
 
 
 
----- PROGRAM ----
+-- COLOR PALETTE
+
+
+darkGrey : String
+darkGrey =
+    "#CCCCCC"
+
+
+mediumGray : String
+mediumGray =
+    "#DDDDDD"
+
+
+lightGrey : String
+lightGrey =
+    "#F3F3F3"
+
+
+highlightBlue : String
+highlightBlue =
+    "#4184F5"
+
+
+
+-- PROGRAM
 
 
 main : Program Never Model Msg
